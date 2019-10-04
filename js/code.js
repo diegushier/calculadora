7551a4806a4c75baaa2d5a04245c37d1f9facca2
Diego Hilderink Dominguez <diego.hilderink@iesdonana.org>
2019-10-04 19:38:27 +0200

    -> Nuevos cambios en el js de la calculadora

--- js/code.js
+++ js/code.js
@@ -1,33 +1,59 @@
+var input = document.getElementbyId("cadena").value;
 var signos = ['x', '/', '+', '-'];
 var aux1 = "";
 var aux2 = "";
 var auxSigno = "";
+var resuelto = 0;
 var encontrado = true;
 
 function lectura(){
-    var input = document.getElementbyId("cadena").value;
-
     do{
         for (i = 0; i < input.length; i++) {
-           if(!novalor(input[i]) && !encontrado){
+           if(!novalor(input[i]) && encontrado==false){
                 aux1+=input[i];
-           } else if(!novalor(input[i]) && encontrado){
-                aux2+=input[i];
+           } else if(!novalor(input[i]) && encontrado==true){
+                segundoValor()
            } else {
                 auxSigno=input[i];
-                encontrado = true;
            }
         }        
     }while(encontrado);
 }
 
+function segundoValor(valor){
+    for (j = i+1; j < input.length; j++) {
+        if(!novalor(input[j])){
+            aux2+=input[j];
+        }else {
+            if(signos.indexOf(input[j]) <= signos.indexOf(auxSigno)){
+                operar(auxSigno);
+            } else {
+                aux1 = aux2;
+                auxSigno = input[i];
+            }
+        }
+    }
+}
+
+function operar(signo){
+    switch(signo){
+        case 'x': resuelto = parseFloat(aux1) * parseFloat(aux2); break;
+        case '/': resuelto = parseFloat(aux1) / parseFloat(aux2); break;
+        case '+': resuelto = parseFloat(aux1) + parseFloat(aux2); break;
+        case '-': resuelto = parseFloat(aux1) - parseFloat(aux2); break;
+        default: console.log("opc no implementada");
+    }
+}
+
+
 function novalor(a){
     for (i = 0; i < signos.length; i++) {
         if (a === signos[i]){
+            encontrado = true;
             return true;
         }
         
     }
-
+    encontrado = false;
     return false;
 }
