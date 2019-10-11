2fc8b7f210d4f5580afa0ead268bb7a6e4174d0e
Diego Hilderink Dominguez <diego.hilderink@iesdonana.org>
2019-10-11 19:44:42 +0200

    Version 1.0 Terminada

--- js/code-2.js
+++ js/code-2.js
@@ -1,54 +1,71 @@
 var input;
 var signos = ['*', '/', '+', '-'];
-var exp = [/\d+\*\d+/, /\d+\/\d+/, /\d+\+\d+/, /\d+\-\d+/];
+var exp = [/(\d+)(\*)(\d+)/, /(\d+)(\/)(\d+)/, /(\d+)(\+)(\d+)/, /(\d+)(\-)(\d+)/];
 var cursor;
-var aux1 = "";
-var aux2 = "";
-var auxSigno = "";
+var aux1;
+var aux2;
+var auxSigno;
 var resuelto;
-var encontrado = false;
 
 
 function lectura(){
-  //Pensar como recorrer todos los caracteres
     input = document.getElementById("ans").value;
     
     for(i = 0; i < exp.length; i++){
         cursor = new RegExp(exp[i]);
-        resuelto = cursor.exec(input);
-        //console.log(input);
-        console.log(resuelto);
-        console.log("Valor 1" + RegExp.$1);
-        console.log("Valor 2" + RegExp.$2);
+        do{
+            resuelto = cursor.exec(input);
+    
+            aux1 = RegExp.$1;
+            auxSigno = RegExp.$2;
+            aux2 = RegExp.$3;
 
+            if (auxSigno === '/' && parseInt(aux2) == 0){
+                input = document.getElementById("ans").value = "";
+                input = document.getElementById("ans").value = "Error aritmético";
+                break;
+            }else{
+                operar();
+                var salida = input.replace(cursor, resuelto);
+                input = salida;
+                //console.log(input);
+                //console.log(" ");                    
+            }
+        } while(cursor.test(input));
+
+        //reseteo de cursores
+        cursor = null;
         resuelto = null;
     }
+
+    //Mostrado por salida
+    aux1 = input;
+    input = document.getElementById("ans").value = "";
+    input = document.getElementById("ans").value = aux1;
+
+    //Reseteo de variables por seguridad
+    cursor = null;
+    resuelto = null;
+    aux1 = null;
+    aux2 = null;
+    auxSigno = null;
 }
-
-//function comparar(caracter){
-//    if(signos.indexOf(caracter) > -1){
-//        return false;
-//    } else{
-//        return true;
-//    }
-//}
-
 
 //Opera según función
 function operar(){
     if (signos.indexOf(auxSigno) == 0){
         resuelto = parseInt(aux1) * parseInt(aux2);
-        console.log(resuelto + " opc1");
+        //console.log(resuelto + " opc1");
     } else if (signos.indexOf(auxSigno) == 1) {
         resuelto = parseInt(aux1) / parseInt(aux2);
-        console.log(resuelto + " opc2");
+        //console.log(resuelto + " opc2");
     } else if (signos.indexOf(auxSigno) == 2) {
         resuelto = parseInt(aux1) + parseInt(aux2);
-        console.log(resuelto + " opc3")
+        //console.log(resuelto + " opc3")
     } else if (signos.indexOf(auxSigno) == 3){
         resuelto = parseInt(aux1) - parseInt(aux2);
-        console.log(resuelto + " opc4")
+        //console.log(resuelto + " opc4")
     } else {
-        console.log("opc no implementada");
+        //console.log("opc no implementada");
     }
 }
